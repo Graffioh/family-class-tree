4a8a747c941e26ce47da60a9dc8a389247cd1815
Umberto Breglia <bregliascuola2002@gmail.com>
2022-03-14 20:17:01 +0100

    Added possibility to write names with spaces

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ I had problems with implementing linked list with template classes cause no know
 So I decided to put the informations inside a vector called familyTree and then print it.
 
 What to implement?
-- Various controls about "correct" input (so for example in the name you can't put a number)
-- Add the possibility to insert a name with spaces
+- Various controls about "correct" input (so for example in the name you can't put a number) (DONE)
+- Add the possibility to insert a name with spaces (DONE)
 - Make Linked list with template classes works with a simple printlist
 - Family heriarchy by adding the node where you want
 - Add a makefile
--- inputValidation.h
+++ inputValidation.h
@@ -4,19 +4,40 @@
 #include <string>
 #include <limits>
 
+
+
 template<typename T>
 void getInput(T& input, const std::string msg)
 {
     std::cout << msg << std::endl;
     std::cin >> input;
 
-    while (std::cin.fail())
+    while (std::cin.fail()) // Input validation using limits library
     {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "Input error,please try again\n";
 
-        std::cout << msg << std::endl;
+        std::cout << msg << std::endl;  
         std::cin >> input;
     }
+}
+
+void getInput(std::string& input, const std::string msg)
+{
+    std::cout << msg << std::endl;
+    std::cin.ignore();
+    std::getline(std::cin,input);
+
+
+    while (std::cin.fail())  // Input validation using limits library
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Input error,please try again\n";
+
+        std::cout << msg << std::endl;  
+        std::getline(std::cin,input);
+    }
+   
 }
